cd61777513559782bf2b6dff024d359f1f4936bb
Dell <Dell@DESKTOP-O2E1PSE>
2020-03-01 00:38:44 -0900

    add two number

--- MySelfRepo/src/MySelfRepo.java
+++ MySelfRepo/src/MySelfRepo.java
@@ -4,6 +4,8 @@ public class MySelfRepo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		 int a=10,b=20;
+		 System.out.println(a+b);
 
 	}
 
